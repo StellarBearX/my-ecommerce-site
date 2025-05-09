50d0fb4afdc1ccc8ac4a5ca4ecf26bd1f79f226a
StellarBearX <kunanan.ws@gmail.com>
2025-05-09 16:17:24 +0700

    Session 6

---
+++ backend/routes/subject.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const subject = require('../data/contact_subject.json');
+
+router.get('/', (req, res) => {
+    // res.end('{"contactSubject": ["General Enquiry", "Class", "Schedule","Instructor","Price","Other"] }');
+    req.json(subject);
+})
+module.exports = router;
--- expServer.js
+++ expServer.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const path = require('path');
+const path = require('path')
 const app = express();
-//serve static file from current project floder
-
+// server static files from current project folder
 app.use(express.static(__dirname));
 
-app.get('/',(req,res)=>{
-res.sendFile(path.join(__dirname,"index.html"))
+app.get('/', (req , res)=> {
+    res.sendFile(path.join(__dirname, "index.html"))
+
 })
-app.listen(3000,()=>{
-    console.log("server is running at http://localhost:3000/")
+
+app.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
 })
